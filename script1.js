35bb14a1e786d17d5a969f906a883e56945c2e5b
aaron-might <aaronmighty7@gmail.com>
2021-05-20 21:36:59 -0400

    update script1.js

--- script1.js
+++ script1.js
@@ -1,1 +1,51 @@
 
+
+var mutltichoices= [
+
+    {
+// Which of the following function of Array object represents the source code of an object?
+
+multichoices:[toSource(), splice(),toString(), unshift()]
+
+answer -toSource() − Represents the source code of an object.
+}
+// Which of the following function of Array object returns the first (least) index of an element within the array equal to the specified value, or -1 if none is found?
+
+multichoices:[indexOf(), join(), lastIndexOf(), map()]
+answer -indexOf() 
+
+// Which of the following function of String object causes a string to be displayed as a subscript, as if it were in a <sub> tag?
+
+mutltichoices:[ sup(), small(), strike(),sub()]
+answer - sub() − 
+
+//4 - Which built-in method sorts the elements of an array?
+
+multichoices:[changeOrder(order), order(), sort(),None of the above.] - 
+
+answer - sort() method sorts the elements of an array.
+
+//5  Which of the following is not considered a JavaScript operator?
+multichoices: [new, this, delete, typeof]
+ Ans: this
+
+ //Using _______ statement is how you test for a specific condition.
+ multichoices: [Select,If, Switch, For]
+  Ans: if
+
+
+
+
+
+
+
+7 Which of the following is true about typeof operator in JavaScript?
+
+ multichoices :[] A - The typeof is a unary operator that is placed before its single operand, which can be of any type.
+
+B - Its value is a string indicating the data type of the operand.
+
+C - Both of the above.
+
+D - None of the above.
+]
